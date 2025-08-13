360d63e6883ee806cba9bce93d7a65994b6f4089
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-08-13 12:44:32 +0500

    start

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://ziyo-tech-server.vercel.app";
+axios.defaults.baseURL = "https://ziyo.test-avtomaktab.uz";
 axios.interceptors.request.use((option) => {
   const token = localStorage.getItem("ziyo-jwt")
     ? localStorage.getItem("ziyo-jwt")
